4ed24586964bcc7decfffb86e12d21d69101648e
Martin Jambon <martin@mjambon.com>
2014-10-13 16:52:10 -0700

    Reporting min/max/median in addition to average and rate for all duration data sent to Cloudwatch.

--- gator_acc.ml
+++ gator_acc.ml
@@ -3,7 +3,7 @@ open Lwt
 type acc1 = (string, int) Hashtbl.t
   (* Counters only, used to compute an average rate only. *)
 
-type acc2 = (string, (int * float)) Hashtbl.t
+type acc2 = (string, float list) Hashtbl.t
   (* Events associated with a value,
      used to compute sampling rate and average value. *)
 
@@ -22,11 +22,33 @@ let add1 {acc1} k =
   Hashtbl.replace acc1 k (v0 + 1)
 
 let add2 {acc2} k v =
-  let n0, v0 =
+  let vl =
     try Hashtbl.find acc2 k
-    with Not_found -> (0, 0.)
+    with Not_found -> []
   in
-  Hashtbl.replace acc2 k (n0 + 1, v0 +. v)
+  Hashtbl.replace acc2 k (v :: vl)
+
+let average l =
+  assert (l <> []);
+  List.fold_left (+.) 0. l /. float (List.length l)
+
+let maxl l =
+  assert (l <> []);
+  List.fold_left max (List.hd l) l
+
+let minl l =
+  assert (l <> []);
+  List.fold_left min (List.hd l) l
+
+let median l =
+  assert (l <> []);
+  let a = Array.of_list l in
+  Array.sort compare a;
+  let n = Array.length a in
+  if n mod 2 = 1 then
+    a.(n/2)
+  else
+    0.5 *. (a.(n/2-1) +. a.(n/2))
 
 let flush_accumulators ~namespace ~period acc =
   add1 acc "gator.flush";
@@ -39,18 +61,20 @@ let flush_accumulators ~namespace ~period acc =
     ) acc.acc1 []
   in
   let jobs2 =
-    Hashtbl.fold (fun k (count, sum) l ->
-      let rate = float count /. period in
-      let average = sum /. float count in
-      let k1 = k ^ ".rate" in
-      let k2 = k ^ ".average" in
-      let job1 =
-        Gator_aws.put_metric_data ~namespace ~metric_name: k1 ~value: rate
+    Hashtbl.fold (fun k vl l ->
+      let data = [
+        k ^ ".rate", float (List.length vl) /. period;
+        k ^ ".average", average vl;
+        k ^ ".median", median vl;
+        k ^ ".min", minl vl;
+        k ^ ".max", maxl vl;
+      ] in
+      let jobs =
+        List.map (fun (k, v) ->
+          Gator_aws.put_metric_data ~namespace ~metric_name: k ~value: v
+        ) data
       in
-      let job2 =
-        Gator_aws.put_metric_data ~namespace ~metric_name: k2 ~value: average
-      in
-      job1 :: job2 :: l
+      jobs @ l
     ) acc.acc2 []
   in
   Hashtbl.clear acc.acc1;
