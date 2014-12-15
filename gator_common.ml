b7cc62936f9d5563b5e55d8bcb8110e3b96d444b
Martin Jambon <martin@mjambon.com>
2014-12-15 12:55:45 -0800

    Relaxing syntax for Cloudwatch metrics

--- gator_common.ml
+++ gator_common.ml
@@ -1,7 +1,7 @@
 (* Utilities shared between server and client *)
 
 let key_rex =
-  let pat = "^[a-z][a-z0-9_]*\\(\\.[a-z][a-z0-9_]*\\)*$" in
+  let pat = "^[a-z0-9_-]+\\(\\.[a-z0-9_-]+\\)*$" in
   Str.regexp pat
 
 let is_valid_key s =
